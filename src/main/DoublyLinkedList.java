c5c2895dc2d4fd8ad28de43699370b4ac03bf4dd
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-05 23:43:08 +0530

    add specifications for attributes of class:Node

--- src/main/DoublyLinkedList.java
+++ src/main/DoublyLinkedList.java
@@ -10,7 +10,11 @@ public class DoublyLinkedList {
 
     /*
     * The 'Node' class for this doubly linked list will have the following attributes
+    * 1. An integer variable to hold the 'data' for the node, named 'data'.
+    * 2. A 'Node' type reference variable to hold the reference of the 'Next Node', named 'next'.
+    * 3. A 'Node' type reference variable to hold the reference of the 'Previous Node', named 'prev'.
     * */
+
 
   }
 
