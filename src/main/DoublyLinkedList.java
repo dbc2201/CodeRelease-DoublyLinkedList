0ce8c4e1fce98e8b4d2754b64aedf92cc3a34315
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-05 23:40:33 +0530

    add comments

--- src/main/DoublyLinkedList.java
+++ src/main/DoublyLinkedList.java
@@ -2,7 +2,15 @@ package main;
 
 public class DoublyLinkedList {
 
+  /*
+  *
+  * */
+
   private static class Node {
+
+    /*
+    * The 'Node' class for this doubly linked list will have the following attributes
+    * */
 
   }
 
