502d061ad2af0320c08292a393461a86add012f1
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-05 23:46:31 +0530

    class:Node; add parametrized constructor

--- src/main/DoublyLinkedList.java
+++ src/main/DoublyLinkedList.java
@@ -3,22 +3,27 @@ package main;
 public class DoublyLinkedList {
 
   /*
-  *
-  * */
+   *
+   * */
 
   private static class Node {
 
     /*
-    * The 'Node' class for this doubly linked list will have the following attributes
-    * 1. An integer variable to hold the 'data' for the node, named 'data'.
-    * 2. A 'Node' type reference variable to hold the reference of the 'Next Node', named 'next'.
-    * 3. A 'Node' type reference variable to hold the reference of the 'Previous Node', named 'prev'.
-    * */
+     * The 'Node' class for this doubly linked list will have the following attributes
+     * 1. An integer variable to hold the 'data' for the node, named 'data'.
+     * 2. A 'Node' type reference variable to hold the reference of the 'Next Node', named 'next'.
+     * 3. A 'Node' type reference variable to hold the reference of the 'Previous Node', named 'prev'.
+     * */
 
     private int data;
     private Node next;
     private Node prev;
 
+    public Node(int data, Node next, Node prev) {
+      this.data = data;
+      this.next = next;
+      this.prev = prev;
+    }
   }
 
 }
