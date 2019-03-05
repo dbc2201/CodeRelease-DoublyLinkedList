199a76ac88aff54134bf665983cf5ceef54f3c0a
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-03-05 23:46:54 +0530

    class:Node; add getters and setters for the attributes

--- src/main/DoublyLinkedList.java
+++ src/main/DoublyLinkedList.java
@@ -24,6 +24,30 @@ public class DoublyLinkedList {
       this.next = next;
       this.prev = prev;
     }
+
+    public int getData() {
+      return data;
+    }
+
+    public void setData(int data) {
+      this.data = data;
+    }
+
+    public Node getNext() {
+      return next;
+    }
+
+    public void setNext(Node next) {
+      this.next = next;
+    }
+
+    public Node getPrev() {
+      return prev;
+    }
+
+    public void setPrev(Node prev) {
+      this.prev = prev;
+    }
   }
 
 }
